c369d46cfda6449463fa58fcfbb6ba0f209e22a9
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-16 15:19:37 +0700

    bai12

---
+++ Bai12/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bai12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bai12")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("486b2240-7c8b-4308-9b3f-38d03129152e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bai12/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai12
+{
+    internal class Student
+    {
+        public int Id;
+        public string Name;
+
+        public Student() { }
+
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Student timSV(List<Student> students, String name)
+        {
+            foreach (Student student in students)
+            {
+                if (student.Name.Equals(name))
+                {
+                    return student;
+                }
+            }
+
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("========================================");
+            Console.WriteLine("NGUYEN LONG NHAT - 23115053122126");
+            Console.WriteLine("========================================");
+
+            List<Student> students = new List<Student>() {
+                new Student(1, "Nhat"),
+                new Student(2, "Nhan"),
+                new Student(3, "Nam")
+            };
+
+            Console.WriteLine("Danh sach sinh vien:");
+            foreach (Student st in students)
+            {
+                Console.WriteLine("ID: " + st.Id + ", Name: " + st.Name);
+            }
+
+            Student student = new Student();
+
+            Console.Write("Nhap ten sinh vien can tim: ");
+            String name = Console.ReadLine();
+            student = student.timSV(students, name);
+
+            if(student != null)
+            {
+                Console.WriteLine("ID: " + student.Id + ", Name: " + student.Name);
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sinh vien");
+            }
+        }
+    }
+}
