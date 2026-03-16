b73d224e0de7b47b67d30bb32660f800fb3ff42f
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-16 15:08:30 +0700

    bai11

---
+++ Bai11/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai11
+{
+    internal class Student
+    {
+
+        public int Id;
+        public string Name;
+
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("NGUYEN LONG NHAT - 23115053122126");
+            Console.WriteLine("========================================");
+
+            List<Student> students = new List<Student>()
+            {
+                new Student(1, "Nhat"),
+                new Student(2, "Nhan"),
+                new Student(3, "Nam")
+            };
+
+            Console.WriteLine("Danh sach sinh vien:");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"ID: {student.Id}, Name: {student.Name}");
+            }
+        }
+    }
+}
